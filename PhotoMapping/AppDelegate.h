9287e6e86717a09bebfdf97c192349b0e1a8fc03
darshan katrumane <kmdarshan@gmail.com>
2015-02-08 15:03:21 -0800

    Initial Commit

---
+++ PhotoMapping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoMapping
+//
+//  Created by kmd on 2/8/15.
+//  Copyright (c) 2015 Happy Days. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoMapping/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PhotoMapping
+//
+//  Created by kmd on 2/8/15.
+//  Copyright (c) 2015 Happy Days. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
